0abe60996bad1e04c5756dc47581f39c2b3843e2
Gerren Keith <gerren.keith23@gmail.com>
2025-06-28 16:36:53 -0500

    Deployment commit

---
+++ ee3459ff20a153dee767.js
@@ -0,0 +1,22 @@
+import  home  from "./home.js"
+import  menu from "./menu.js"
+import  about  from "./about.js";
+const homePage = document.getElementById('home');
+const menuPage = document.getElementById('menu');
+const aboutPage = document.getElementById('about');
+const content = document.getElementById('content');
+
+
+const handleClick = () => {
+    let isClicked = false;
+    if(!isClicked){
+        content.innerHTML = '';
+        isClicked = true
+    }
+
+}
+
+homePage.addEventListener('click',()=>{handleClick(); home()});
+menuPage.addEventListener('click', ()=>{handleClick(); menu()});
+aboutPage.addEventListener('click', ()=>{handleClick(); about()});
+
